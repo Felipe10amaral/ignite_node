eb8a3e3925b7d6a33f7a906fa1227da3e492a826
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-02-18 17:14:14 -0300

    criando a primeira migration

--- Projeto 2/src/database.ts
+++ Projeto 2/src/database.ts
@@ -1,8 +1,16 @@
-import knex from 'knex';
+import {knex, Knex} from 'knex';
 
-export const nex = knex({
+export const config: Knex.Config = {
     client: 'sqlite',
     connection: {
-        filename: './tmp/app.db'
+        filename: './db/app.db'
+    },
+
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations'
     }
-});
+}
+
+export const nex = knex(config);
